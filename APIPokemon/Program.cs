709f80849e36c49db03a6a6e567bb8ffe70b16f7
João Pedro Guedes <guedesjoaopedro700@gmail.com>
2024-11-26 22:35:17 -0300

    Conexão com o client retornando um Json com os Pokemons

--- APIPokemon/Program.cs
+++ APIPokemon/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Olá, Mundo!");
+﻿using RestSharp;
+using System;
+using System.Reflection.Metadata.Ecma335;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var client = new RestClient($"https://pokeapi.co/api/v2/");
+
+        var request = new RestRequest("pokemon", Method.Get);
+
+        var response = client.Execute(request);
+        try
+        {
+            Console.WriteLine(response.Content);
+
+        }catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
